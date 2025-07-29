bb7123e50a59ce61109b335d181e538985839431
Manoel Estevam Neto <104177851+estevam02009@users.noreply.github.com>
2025-07-29 17:18:54 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753820333186

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_724ee4a4._.js",
+  "static/chunks/src_94935e4e._.js"
+],
+    source: "dynamic"
+});
